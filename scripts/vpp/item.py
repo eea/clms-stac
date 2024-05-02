1a7cc692a0b04d191816ddaf026d3b246b9a8406
chorng <chung.horng@sinergise.com>
2024-05-02 14:00:52 +0200

    Refactor vpp item creation script
    * add create_asset_href function
    
    * break create_item to small functions
    
    * add boto core error and rasterio error to error handler

--- scripts/vpp/item.py
+++ scripts/vpp/item.py
@@ -9,12 +9,14 @@ from datetime import datetime
 import boto3
 import pystac
 import rasterio as rio
+from botocore.exceptions import BotoCoreError
 from botocore.paginate import PageIterator
 from jsonschema import Draft7Validator
 from jsonschema.exceptions import best_match
 from pystac.extensions.projection import ProjectionExtension
 from rasterio.coords import BoundingBox
 from rasterio.crs import CRS
+from rasterio.errors import RasterioIOError
 from rasterio.warp import transform_bounds
 from referencing import Registry, Resource
 from shapely.geometry import Polygon, box, mapping
@@ -47,7 +49,7 @@ def create_product_list(start_year: int, end_year: int) -> list[str]:
 def create_page_iterator(aws_session: boto3.Session, bucket: str, prefix: str) -> PageIterator:
     client = aws_session.client("s3")
     paginator = client.get_paginator("list_objects_v2")
-    return paginator.paginate(Bucket=bucket, Prefix=prefix, Delimiter="-")
+    return paginator.paginate(Bucket=bucket, Prefix=prefix, Delimiter="-", MaxKeys=10)
 
 
 def read_metadata_from_s3(bucket: str, key: str, aws_session: boto3.Session) -> tuple[BoundingBox, CRS, int, int]:
@@ -79,15 +81,62 @@ def get_datetime(product_id: str) -> tuple[datetime, datetime]:
     return (datetime(year=year, month=1, day=1), datetime(year=year, month=12, day=31))
 
 
+def create_asset_href(bucket: str, asset_key: str) -> str:
+    return f"s3://{bucket}/" + asset_key
+
+
 def create_asset(asset_key: str) -> pystac.Asset:
     parameter = asset_key.split("_")[-1].split(".")[0]
     version = asset_key.split("_")[-3]
+    href = create_asset_href(BUCKET, asset_key)
     return pystac.Asset(
-        href=f"s3://{BUCKET}/" + asset_key,
+        href=href,
         media_type=pystac.MediaType.GEOTIFF,
         title=TITLE_MAP[parameter] + f" {version}",
         roles=["data"],
     )
+
+
+def create_core_item(
+    product_id: str,
+    geometry: Polygon,
+    start_datetime: datetime,
+    end_datetime: datetime,
+    create_datetime: datetime,
+    description: str,
+    collection: str,
+):
+    return pystac.Item(
+        id=product_id,
+        geometry=mapping(geometry),
+        bbox=list(geometry.bounds),
+        datetime=None,
+        start_datetime=start_datetime,
+        end_datetime=end_datetime,
+        properties={"created": create_datetime.strftime("%Y-%m-%dT%H:%M:%SZ"), "description": description},
+        collection=collection,
+    )
+
+
+def add_providers_to_item(item: pystac.Item, provider_list: list[pystac.Provider]) -> None:
+    item.common_metadata.providers = provider_list
+
+
+def add_projection_extension_to_item(item: pystac.Item, crs: CRS, bounds: BoundingBox, height: int, width: int) -> None:
+    projection = ProjectionExtension.ext(item, add_if_missing=True)
+    projection.epsg = crs.to_epsg()
+    projection.bbox = [int(bounds.left), int(bounds.bottom), int(bounds.right), int(bounds.top)]
+    projection.shape = [height, width]
+
+
+def add_links_to_item(item: pystac.Item, link_list: list[pystac.Link]) -> None:
+    for link in link_list:
+        item.links.append(link)
+
+
+def add_assets_to_item(item: pystac.Item, asset_dict: dict[str, pystac.Asset]) -> None:
+    for key, asset in asset_dict.items():
+        item.add_asset(key, asset)
 
 
 def create_item(aws_session: boto3.Session, bucket: str, tile: str) -> pystac.Item:
@@ -101,34 +150,23 @@ def create_item(aws_session: boto3.Session, bucket: str, tile: str) -> pystac.It
     description = get_description(product_id)
     start_datetime, end_datetime = get_datetime(product_id)
 
+    # core metadata
+    item = create_core_item(product_id, geom_wgs84, start_datetime, end_datetime, created, description, COLLECTION_ID)
+
     # common metadata
-    item = pystac.Item(
-        id=product_id,
-        geometry=mapping(geom_wgs84),
-        bbox=list(geom_wgs84.bounds),
-        datetime=None,
-        start_datetime=start_datetime,
-        end_datetime=end_datetime,
-        properties={"created": created.strftime("%Y-%m-%dT%H:%M:%SZ"), "description": description},
-        collection=COLLECTION_ID,
-    )
-    item.common_metadata.providers = [VPP_HOST_AND_LICENSOR, VPP_PRODUCER_AND_PROCESSOR]
+    provider_list = [VPP_HOST_AND_LICENSOR, VPP_PRODUCER_AND_PROCESSOR]
+    add_providers_to_item(item, provider_list)
 
     # extensions
-    projection = ProjectionExtension.ext(item, add_if_missing=True)
-    projection.epsg = crs.to_epsg()
-    projection.bbox = [int(bounds.left), int(bounds.bottom), int(bounds.right), int(bounds.top)]
-    projection.shape = [height, width]
+    add_projection_extension_to_item(item, crs, bounds, height, width)
 
     # links
-    links = [CLMS_LICENSE, CLMS_CATALOG_LINK, ITEM_PARENT_LINK, COLLECTION_LINK]
-    for link in links:
-        item.links.append(link)
+    link_list = [CLMS_LICENSE, CLMS_CATALOG_LINK, ITEM_PARENT_LINK, COLLECTION_LINK]
+    add_links_to_item(item, link_list)
 
     # assets
     assets = {os.path.split(key)[-1][:-4].lower(): create_asset(key) for key in asset_keys}
-    for key, asset in assets.items():
-        item.add_asset(key, asset)
+    add_assets_to_item(item, assets)
     return item
 
 
@@ -142,11 +180,11 @@ def get_stac_validator(product_schema: str) -> Draft7Validator:
 
 
 def create_vpp_item(aws_session: boto3.Session, bucket: str, validator: Draft7Validator, tile: str) -> None:
-    item = create_item(aws_session, bucket, tile)
-    item.set_self_href(os.path.join(WORKING_DIR, f"{STAC_DIR}/{COLLECTION_ID}/{item.id}/{item.id}.json"))
-    error_msg = best_match(validator.iter_errors(item.to_dict()))
     try:
+        item = create_item(aws_session, bucket, tile)
+        item.set_self_href(os.path.join(WORKING_DIR, f"{STAC_DIR}/{COLLECTION_ID}/{item.id}/{item.id}.json"))
+        error_msg = best_match(validator.iter_errors(item.to_dict()))
         assert error_msg is None, f"Failed to create {item.id} item. Reason: {error_msg}."
         item.save_object()
-    except AssertionError as error:
+    except (AssertionError, BotoCoreError, RasterioIOError) as error:
         LOGGER.error(error)
