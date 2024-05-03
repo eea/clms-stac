ac440ecf5325c96bba5dd3a6ae42d425aa9db3e5
Sebastian Boeck <sebastian.boeck@umweltbundesamt.at>
2024-05-03 16:42:15 +0200

    collection creation split in two parts

--- create_clc_collection.py
+++ create_clc_collection.py
@@ -1,9 +1,10 @@
 import logging
 
-from scripts.clc.collection import create_collection
+from scripts.clc.collection import create_collection, populate_collection
 
 LOGGER = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     logging.basicConfig(filename="create_clc_collection.log")
-    create_collection("<path-to-clc-root-directory>")
+    collection = create_collection()
+    populate_collection(collection, data_root="../CLC_100m")
