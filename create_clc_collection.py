402b35d4c4f7c21541451ffcf06301b07198f4eb
Sebastian Boeck <sebastian.boeck@umweltbundesamt.at>
2024-05-06 15:33:45 +0200

    introduces main()

--- create_clc_collection.py
+++ create_clc_collection.py
@@ -4,7 +4,11 @@ from scripts.clc.collection import create_collection, populate_collection
 
 LOGGER = logging.getLogger(__name__)
 
-if __name__ == "__main__":
+def main():
     logging.basicConfig(filename="create_clc_collection.log")
     collection = create_collection()
     populate_collection(collection, data_root="../CLC_100m")
+
+
+if __name__ == "__main__":
+    main()
