a5b34897c30c1f78e5fb307ade69dab91505331e
Sebastian Boeck <sebastian.boeck@umweltbundesamt.at>
2024-05-17 15:30:59 +0200

    adjusts deconstruct_clc_name and file listing and asset creation for CLC+

--- scripts/clc-plus/item.py
+++ scripts/clc-plus/item.py
@@ -11,18 +11,22 @@ import rasterio.warp
 from pystac.extensions.projection import ProjectionExtension
 from shapely.geometry import box, mapping
 
-from .constants import (
-    CLC_PROVIDER,
-    CLMS_LICENSE,
-    COLLECTION_ID,
-    DOM_MAP,
-    ITEM_DESCRIPTION,
-    ITEM_MEDIA_TYPE_MAP,
-    ITEM_ROLES_MAP,
-    ITEM_TITLE_MAP,
-    STAC_DIR,
-    WORKING_DIR,
-)
+# from .constants import (
+#     CLC_PROVIDER,
+#     CLMS_LICENSE,
+#     COLLECTION_ID,
+#     DOM_MAP,
+#     ITEM_DESCRIPTION,
+#     ITEM_MEDIA_TYPE_MAP,
+#     ITEM_ROLES_MAP,
+#     ITEM_TITLE_MAP,
+#     STAC_DIR,
+#     WORKING_DIR,
+# )
+
+import sys
+sys.path.append(os.path.abspath('clms-stac/scripts/clc-plus/'))
+from constants import *
 
 LOGGER = logging.getLogger(__name__)
 
@@ -36,33 +40,35 @@ def deconstruct_clc_name(filename: str) -> dict[str]:
         filename_split |= m.groupdict()
 
     p = re.compile(
-        "U(?P<update_campaign>[0-9]{4})_"
-        "(?P<theme>CLC|CHA)(?P<reference_year>[0-9]{4})_"
-        "V(?P<release_year>[0-9]{4})_(?P<release_number>[0-9a-z]*)"
-        "_?(?P<country_code>[A-Z]*)?"
-        "_?(?P<DOM_code>[A-Z]*)?"
+        "CLMS_CLCplus_"
+        "(?P<product_acronym>[A-Z]{6})_"
+        "(?P<reference_year>[0-9]{4})_"
+        "(?P<resolution>[0-9a-z]{4})_"
+        "(?P<extent>[0-9A-Za-z]{2,5})_"
+        "(?P<epsg>[0-9]*)_"
+        "(?P<version>V[0-9]_[0-9])"
     )
     m = p.search(filename_split["id"])
 
     if m:
         filename_split |= m.groupdict()
-
     return filename_split
 
 
-def create_item_asset(asset_file: str, dom_code: str) -> pystac.Asset:
+
+def create_item_asset(asset_file: str, extent: str) -> tuple[str, pystac.Asset]:
     filename_elements = deconstruct_clc_name(asset_file)
 
-    suffix = filename_elements["suffix"].replace(".", "_")
+    key = filename_elements["suffix"].replace(".", "_")
 
-    if id.startswith("readme"):
-        key = "readme_" + suffix
-    elif id.endswith("QGIS"):
-        key = "legend_" + suffix
-    else:
-        key = suffix
+    # if id.startswith("readme"):
+    #     key = "readme_" + suffix
+    # elif id.endswith("QGIS"):
+    #     key = "legend_" + suffix
+    # else:
+    #     key = suffix
 
-    label = DOM_MAP[dom_code]
+    label = EXTENT_MAP[extent]
 
     asset = pystac.Asset(
         href=asset_file,
@@ -70,13 +76,14 @@ def create_item_asset(asset_file: str, dom_code: str) -> pystac.Asset:
         media_type=ITEM_MEDIA_TYPE_MAP[key],
         roles=ITEM_ROLES_MAP[key],
     )
-    return f"{filename_elements['id']}_{suffix}", asset
+
+    return ITEM_KEY_MAP[key], asset
 
 
 def get_img_paths(data_root: str) -> list[str]:
     img_paths = []
     for root, _, files in os.walk(data_root):
-        if root.endswith(("DATA", "French_DOMs")):
+        if "Data" in root:
             for file in files:
                 if file.endswith(".tif"):
                     img_paths.append(os.path.join(root, file))
@@ -87,26 +94,26 @@ def get_img_paths(data_root: str) -> list[str]:
 def get_item_asset_files(data_root: str, img_path: str) -> list[str]:
     clc_name_elements = deconstruct_clc_name(img_path)
     clc_id = clc_name_elements["id"]
-    dom_code = clc_name_elements["DOM_code"]
+    clc_extent = clc_name_elements["extent"]
 
     asset_files = []
 
     for root, _, files in os.walk(data_root):
-        if not dom_code and "French_DOMs" in root:
-            continue
+        # if not clc_extent and "French_DOMs" in root:
+        #     continue
 
-        if dom_code and "Legend" in root and "French_DOMs" not in root:
-            continue
+        # if clc_extent and "Legend" in root and "French_DOMs" not in root:
+        #     continue
 
-        if "U{update_campaign}_{theme}{reference_year}_V{release_year}".format(**clc_name_elements).lower() not in root:
-            continue
+        # if "U{update_campaign}_{theme}{reference_year}_V{release_year}".format(**clc_name_elements).lower() not in root:
+        #     continue
 
         for file in files:
             if (
                 file.startswith(f"{clc_id}.")
                 or file.endswith(
                     (
-                        f"{dom_code}.tif.lyr",
+                        f"{clc_extent}.tif.lyr",
                         "QGIS.txt",
                     )
                 )
@@ -151,7 +158,7 @@ def create_item(img_path: str, data_root: str) -> pystac.Item:
     }
 
     with rio.open(img_path) as img:
-        if clc_name_elements["DOM_code"]:
+        if clc_name_elements["extent"] != 'eu':
             bbox = project_bbox(img, dst_crs=rio.CRS.from_epsg(4326))
         else:
             bbox = project_data_window_bbox(img, dst_crs=rio.CRS.from_epsg(4326))
@@ -170,7 +177,7 @@ def create_item(img_path: str, data_root: str) -> pystac.Item:
 
     for asset_file in asset_files:
         try:
-            key, asset = create_item_asset(asset_file, DOM_code=clc_name_elements.get("DOM_code"))
+            key, asset = create_item_asset(asset_file, extent=clc_name_elements.get("extent"))
             item.add_asset(
                 key=key,
                 asset=asset,
